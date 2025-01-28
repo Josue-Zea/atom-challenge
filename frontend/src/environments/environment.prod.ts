495f45420ee34fd1926b0d30fe2e597a1ddd66c4
Josue Zea <jdzeaherrera@gmail.com>
2025-01-27 23:37:42 -0600

    frontend structure

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  port: '80',
+  backend: 'http://localhost:3000',
+  contexts: {
+    tasks: 'tasks',
+    users: 'users',
+  },
+};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}", // Escanea los archivos HTML y TypeScript en src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
