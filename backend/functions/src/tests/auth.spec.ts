881b841393188594ec5ce3c3a08ee413c9bab0cd
Josue Zea <jdzeaherrera@gmail.com>
2025-01-29 02:29:33 -0600

    Full design ready to deploy

---
+++ backend/functions/src/tests/auth.spec.ts
@@ -0,0 +1,18 @@
+// import test, { describe } from "node:test";
+// import { checkUserExists, createUser } from "../services/auth.service";
+
+
+// describe("User Service", () => {
+//     test("should return a user if it exists", async () => {
+//         const email = "test@example.com";
+//         const user = await checkUserExists(email);
+//         expect(user).toBeDefined();
+//     });
+
+//     test("should create a new user", async () => {
+//         const email = "newuser@example.com";
+//         const newUser = await createUser(email);
+//         eepect(newUser.email).toBe(email);
+//         expect(newUser.id).toBeDefined();
+//     });
+// });
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   port: '80',
-  backend: 'http://localhost:3000',
+  backend: 'https://us-central1-atom-challenge-c5867.cloudfunctions.net/app',
   services: {
     tasks: 'tasks',
     users: 'users',
